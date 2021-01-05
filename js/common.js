59a30d5692f875669e32f3f6961b0117241895c0
dkstndus <73566287+dkstndus@users.noreply.github.com>
2021-01-05 14:59:55 +0900

    update
    update

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(function(){
+  // skill count
   $(".flex_box .count").each(function(){
     $(this).prop('Counter',0).animate({Counter: $(this).text()},{
       duration: 4000,
@@ -9,10 +10,19 @@ $(function(){
     });
   });
 
+ //popup
   $(".nav_contact a").click(function(){
     $("#contact_popup").fadeIn().css("display","block");
     $(".close_btn").click(function(){
       $("#contact_popup").css("display","none");
     });
   });
+
+  //portfolio list순서바꾸기
+
+  // $("#nav .nav_list li:nth-of-type(4) a").click(function(){
+  //   $(".list_wrap").prepend('<div class="list_inner"><div class="list"><a href="portfolio02.html"><img src="images/images/img_thum03.jpg" alt="portfolio_img_thum03"></a><div class="list_name"><p><a href="portfolio02.html">GURM BY H</a></p><div class="list_file"><P>HTML</P><P>CSS</P><P>SASS</P></div></div></div></div>');
+  // });
+
+
 });
